54d9171b53f751b975a999e71cf0194316bdd6bc
ukidabek <ukidabek@gmail.com>
2018-03-08 20:18:59 +0100

    - Path added.

---
+++ Path.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Track
+{
+	[Serializable]
+	public class Path
+	{
+        [SerializeField, HideInInspector]
+        private List<Vector3> _points = new List<Vector3>();
+    }
+}
---
+++ Track.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Track
+{
+	public class Track : MonoBehaviour 
+	{
+        [SerializeField, HideInInspector]
+        private Path _path = new Path();
+    }
+}
